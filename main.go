5bc187121234f674308a8e1378a72b03c7ad5bae
Michael Hartley <michaelhartley@Michaels-MacBook-Pro.local>
2019-03-19 19:34:53 -0700

    refactored transactoin logic

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
+)
+
+type CategoryFilter string
+
+const (
+	Bills         CategoryFilter = "Bills & Utilities"
+	Entertainment CategoryFilter = "Entertainment"
+	Food          CategoryFilter = "Food & Drink"
+	Gas           CategoryFilter = "Gas"
+	Groceries     CategoryFilter = "Groceries"
+	Home          CategoryFilter = "Home"
+	Personal      CategoryFilter = "Personal"
+	Shopping      CategoryFilter = "Shopping"
+	Travel        CategoryFilter = "Travel"
 )
 
 type Transaction struct {
@@ -32,8 +44,8 @@ func returnFloat(s string) float64 {
 	return num
 }
 
-func Filter(t Transaction, filterFunc func(t Transaction) bool) bool {
-	if filterFunc(t) {
+func Filter(t Transaction, categoryFilter CategoryFilter, filterFunc func(t Transaction, categoryFilter CategoryFilter) bool) bool {
+	if filterFunc(t, categoryFilter) {
 		return true
 	}
 	return false
@@ -47,24 +59,50 @@ func (tl *TransactionList) sum() float64 {
 	return sum
 }
 
-func foodFilter(t Transaction) bool {
-	fmt.Println("within foodFilter")
-	fmt.Println(len(strings.TrimSpace(t.Category)))
-	fmt.Println(len("Food & Drink"))
-	return t.Category == "Food & Drink"
+func sumTransactions(transactions []Transaction) float64 {
+	sum := 0.0
+	for _, transaction := range transactions {
+		sum += transaction.Amount
+	}
+	return sum
 }
+
+func transactionFilter(t Transaction, categoryFilter CategoryFilter) bool {
+	return t.Category == string(categoryFilter)
+}
+
+func numCategories(transactions []Transaction, categoryFilter CategoryFilter) int {
+	count := 0
+	for _, transaction := range transactions {
+		if transactionFilter(transaction, categoryFilter) {
+			count++
+		}
+	}
+	return count
+}
+
+func createCategorizedTransactionsList(transactions []Transaction, categoryFilter CategoryFilter) []Transaction {
+	var categorizedTransactions []Transaction
+	for _, transaction := range transactions {
+		if transactionFilter(transaction, categoryFilter) {
+			categorizedTransactions = append(categorizedTransactions, transaction)
+		}
+	}
+	return categorizedTransactions
+}
+
 func main() {
 	fmt.Println("Hello world")
 
 	f, _ := os.Open("sheets/Chase0588_Activity20190319.CSV")
-	// reader := csv.NewReader(bufio.NewReader(f))
-	var transactions []Transaction
 
 	lines, err := csv.NewReader(f).ReadAll()
 
 	if err != nil {
 		panic(err)
 	}
+
+	var allTransactions []Transaction
 
 	for _, line := range lines {
 		data := Transaction{
@@ -74,19 +112,27 @@ func main() {
 			Type:        line[4],
 			Amount:      returnFloat(line[5]),
 		}
-		fmt.Println(data.Category)
+		if data.Amount > 0 {
+			continue //Filter out paying off credit card
+		}
+		allTransactions = append(allTransactions, data)
 
-		if Filter(data, foodFilter) {
-			transactions = append(transactions, data)
-		}
-		out, err := json.Marshal(data)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(string(out))
 	}
-	fmt.Println(len(transactions))
-	tl := NewTransactionList(transactions)
-	sum := tl.sum()
-	fmt.Println(sum)
+
+	// billTransactions := createCategorizedTransactionsList(allTransactions, Bills)
+	// entertainmentTransactions := createCategorizedTransactionsList(allTransactions, Entertainment)
+	// foodTransactions := createCategorizedTransactionsList(allTransactions, Food)
+	// gasTransactions := createCategorizedTransactionsList(allTransactions, Gas)
+	groceriesTransactions := createCategorizedTransactionsList(allTransactions, Groceries)
+	// homeTransactions := createCategorizedTransactionsList(allTransactions, Home)
+	// personalTransactions := createCategorizedTransactionsList(allTransactions, Personal)
+	// shoppingTransactions := createCategorizedTransactionsList(allTransactions, Shopping)
+	// travelTransactions := createCategorizedTransactionsList(allTransactions, Travel)
+
+	fmt.Println(groceriesTransactions)
+	groceriesMoney := sumTransactions(groceriesTransactions)
+	groceriesTimes := numCategories(allTransactions, Groceries)
+	fmt.Println(groceriesMoney)
+	fmt.Println(groceriesTimes)
+	fmt.Println(len(groceriesTransactions))
 }
